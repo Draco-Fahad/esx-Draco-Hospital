8e5f3f74c89ea7a9073b28c5d61f1795ea5401f2
Draco-Fahad <84138389+Draco-Fahad@users.noreply.github.com>
2021-05-13 19:10:43 +0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.EnableBlips = false
+Config.EnablePeds = true
+
+Config.DoctorLimit = false
+Config.maxDoctor = 0
+Config.doctorPrice = 5000
+Config.Doctor = {
+    {x = 328.74, y = -577.41, z = 43.32, heading = 168.56, type = 'legal'}
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "adamant"
+game "gta5"
+Author "Draco#7539"
+
+description 'Draco-Customs'
+
+client_scripts {
+    'client/client.lua',
+    'config.lua'
+}
+
+server_scripts {
+    'server/server.lua',
+    'config.lua'
+}
